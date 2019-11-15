eb42a537ff85742aa1adf6802345a178abd882f9
Priscila <priscilaresantos@gmail.com>
2019-11-15 01:24:50 -0300

    Livroapp
    Criando as classes , serviçoes  para consumir da web api

--- LivroApp/src/app/app.module.ts
+++ LivroApp/src/app/app.module.ts
@@ -1,8 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { LivroComponent } from './livro/livro.component';
+import { ClienteService } from './livro.service';  
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'; 
+import { HttpClientModule, HttpClient } from '@angular/common/http'; 
+import {  
+  MatButtonModule, MatMenuModule, MatDatepickerModule,MatNativeDateModule , MatIconModule, 
+  MatCardModule, MatSidenavModule,MatFormFieldModule,  
+  MatInputModule, MatTooltipModule, MatToolbarModule  
+  } from '@angular/material';  
+  
+  import { MatRadioModule } from '@angular/material/radio';  
+  import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,9 +22,26 @@ import { LivroComponent } from './livro/livro.component';
     LivroComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,  
+    ReactiveFormsModule,  
+    HttpClientModule,  
+    BrowserAnimationsModule,  
+    MatButtonModule,  
+    MatMenuModule,  
+    MatDatepickerModule,  
+    MatNativeDateModule,  
+    MatIconModule,  
+    MatRadioModule,  
+    MatCardModule,  
+    MatSidenavModule,  
+    MatFormFieldModule,  
+    MatInputModule,  
+    MatTooltipModule,  
+    MatToolbarModule,  
+    AppRoutingModule  
   ],
-  providers: [],
+  providers: [HttpClientModule, LivroService,MatDatepickerModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- LivroApp/src/app/livro/livro.component.ts
+++ LivroApp/src/app/livro/livro.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Livro } from '../livro';
+import { Observable } from 'rxjs';
+import { LivroService } from '../livro.service';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-livro',
@@ -7,9 +11,82 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LivroComponent implements OnInit {
 
-  constructor() { }
+  dataSaved = false;  
+  livroForm: any;  
+  allLivros: Observable<Livro[]>;  
+  livroIdUpdate = null;  
+  massage = null;  
+  constructor(private formbulider: FormBuilder, private livroService:LivroService) { }
 
-  ngOnInit() {
-  }
+  ngOnInit() {  
+    this.livroForm = this.formbulider.group({  
+      Titulo: ['', [Validators.required]],  
+      Autor: ['', [Validators.required]],  
+      AnoEdicao: ['', [Validators.required]], 
+    });  
+    this.loadAllLivros();  
+   }
 
-}
+   loadAllLivros() {  
+      this.allLivros = this.livroService.getAllLivro();  
+   }     
+
+   onFormSubmit() {  
+      this.dataSaved = false;  
+      const livro = this.livroForm.value;  
+      this.CreateLivro(livro);  
+      this.livroForm.reset();  
+   }    
+
+   loadLivroToEdit(livroId: string) {  
+      this.livroService.getLivroById(livroId).subscribe(livro=> {  
+        this.massage = null;  
+        this.dataSaved = false;  
+        this.livrodUpdate = livro.Id;  
+        this.livroForm.controls['Titulo'].setValue(livro.Titulo);  
+        this.livroForm.controls['Autor'].setValue(livro.Autor);  
+        this.livroForm.controls['AnoEdicao'].setValue(livro.AnoEdicao);  
+      });  
+    }  
+
+    CreateLivro(livro: Livro) {  
+      if (this.livroIdUpdate == null) {  
+        this.livroService.createLivro(livro).subscribe(  
+          () => {  
+            this.dataSaved = true;  
+            this.massage = 'Registro salvo com sucesso';  
+            this.loadAllLivros();  
+            this.livroIdUpdate = null;  
+            this.livroForm.reset();  
+          }  
+        );  
+      } else {  
+        livro.Id = this.livroIdUpdate;  
+        this.livroService.updateCliente(this.livroIdUpdate, livro).subscribe(() => {  
+          this.dataSaved = true;  
+          this.massage = 'Registro Atualizado com sucesso';  
+          this.loadAllLivros();  
+          this.livroIdUpdate = null;  
+          this.livroForm.reset();  
+        });  
+      }  
+    }   
+
+    deleteLivro(livroId: string) {  
+      if (confirm("Deseja realmente excluir este livro ?")) {   
+      this.livroService.deleteLivroById(livroId).subscribe(() => {  
+        this.dataSaved = true;  
+        this.massage = 'Operação realizada com sucesso';  
+        this.loadAllLivros();  
+        this.livroIdUpdate = null;  
+        this.livroForm.reset();  
+      });  
+    }  
+  }  
+
+  resetForm() {  
+      this.livroForm.reset();  
+      this.massage = null;  
+      this.dataSaved = false;  
+    }  
+  }  
